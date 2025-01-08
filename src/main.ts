98ce7bc8a176be5e920059a55fd32351e2dd7e36
tuantt <tuantt@cyberk.io>
2025-01-08 22:52:57 +0700

    feat: blogs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express'; // Cập nhật import
 
-const server = express();
+const server = express(); // Khởi tạo server Express
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
