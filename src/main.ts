8dd4bcedabbf810815f8bdae95b8e78eb4070f80
tuantt <tuantt@cyberk.io>
2025-01-08 20:36:39 +0700

    feat: blogs

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import * as cors from 'cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cors({
-      origin: ['http://localhost:3000', 'https://handi-eco.vn'], // e.g., 'http://localhost:3000'
-      credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    }),
-  );
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://handi-eco.vn'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
