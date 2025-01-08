8a1cf70deedfe74a6a45457f3aca88e95f3bb14f
tuantt <tuantt@cyberk.io>
2025-01-08 22:27:20 +0700

    feat: blogs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
       'http://localhost:3000',
@@ -20,5 +16,3 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
-
-export default server; // Xuất server để Vercel sử dụng
